b96f59a0c6afc7c385edb9494e49acb01c586818
Abhishek-Rathi-LV <abhishekrathilv@gmail.com>
2026-09-04 13:43:31 +0530

    Add files via upload

---
+++ 08.py
@@ -0,0 +1,14 @@
+marks = float(input("Enter the student's marks: "))
+
+if marks >= 90:
+    grade = "A+"
+elif marks >= 75:
+    grade = "A"
+elif marks >= 60:
+    grade = "B"
+elif marks >= 50:
+    grade = "C"
+else:
+    grade = "F"
+
+print("The student's grade is:", grade)
